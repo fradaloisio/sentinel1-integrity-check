eb88854bc04c3040a7a8033830b58c0c30dc36ff
Francesco d'Aloisio <fdaloisio@gmail.com>
2015-04-15 11:40:01 +0200

    added verbose output and file check

--- s1_check.py
+++ s1_check.py
@@ -3,20 +3,34 @@ import libxml2
 import sys
 import os.path
 
+verbose = False
+
 try:
-    product = str(sys.argv[1])
+    if str(sys.argv[1]) == "-v":
+        product = str(sys.argv[2])
+        verbose = True
+    else:
+        product = str(sys.argv[1])
 except IndexError:
     print "Product is missing\n" \
           "USAGE:\n" \
-          "python2.7 s1_check.py PRODUCT_NAME.SAFE"
+          "python2.7 s1_check.py PRODUCT_NAME.SAFE\n" \
+          "Options:\n" \
+          "-v : Verbose"
     exit()
+
+def report(message):
+    if verbose:
+        print message
 
 if not os.path.isdir(product):
     print "[ERROR] " + product + " isn't a folder"
+    print "False"
     exit()
 
 if not os.path.isfile(product+"/manifest.safe"):
-    print "[ERROR] " + product+"/manifest.safe" + " NOT EXIST"
+    report( "[ERROR] " + product+"/manifest.safe" + " NOT EXIST" )
+    print "False"
     exit()
 
 try:
@@ -37,16 +51,29 @@ try:
         filename_checksum = str(manifest.xpathEval('byteStream/checksum')[0].content)
         full_path = product+filename[1:]
 
-        checksum = hashlib.md5(open(full_path, 'rb').read()).hexdigest()
-
-        if checksum != filename_checksum:
-            print "[ERROR]" + product+filename[1:] + " not valid"
+        if not os.path.isfile(full_path):
+            report( "[ERROR] " + full_path + " NOT EXIST" )
             error = True
+            print "False"
+            exit()
+        else:
+            checksum = hashlib.md5(open(full_path, 'rb').read()).hexdigest()
+
+            if checksum != filename_checksum:
+                report( "[ERROR]" + product+filename[1:] + " NOT VALID" )
+                error = True
+                print "False"
+                exit()
 
     if error:
-        print "[ERROR] " + product + " NOT OK"
+        report( "[ERROR] " + product + " NOT OK" )
+        print "False"
+        exit()
     else:
-        print product + " OK"
+        report( product + " OK" )
+        print "True"
+        exit()
 
 except libxml2.parserError:
-    print "manifest not valid ... "
+    report("manifest not valid ... ")
+    exit()
