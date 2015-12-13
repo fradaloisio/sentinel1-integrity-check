cb945176f3cadf00edcdd7ec6dc0898d577701e3
fradaloisio <fdaloisio@gmail.com>
2015-12-13 18:24:15 +0100

    test

--- s1_check.py
+++ s1_check.py
@@ -3,7 +3,15 @@ import libxml2
 import sys
 import os.path
 
+from Queue import Queue
+from threading import Thread
+import time
+
 verbose = False
+error = False
+filesQ = Queue()
+
+num_threads = 4
 
 try:
     if str(sys.argv[1]) == "-v":
@@ -23,8 +31,33 @@ def report(message):
     if verbose:
         print message
 
+
+def checkMD5(q):
+    global error
+    while error != True:
+        full_path = q.get()
+        print(full_path)
+        if not os.path.isfile(full_path):
+            report( "[ERROR] " + full_path + " NOT EXIST" )
+            error = True
+            print "False"
+            exit()
+        else:
+            checksum = hashlib.md5(open(full_path, 'rb').read()).hexdigest()
+
+            print(checksum)
+            print(filename_checksum)
+
+            if checksum != filename_checksum:
+                report( "[ERROR]" + full_path + " NOT VALID" )
+                error = True
+                print "False"
+                exit()
+            q.task_done()
+
+
 if not os.path.isdir(product):
-    print "[ERROR] " + product + " isn't a folder"
+    report( "[ERROR] " + product + " isn't a folder")
     print "False"
     exit()
 
@@ -32,6 +65,14 @@ if not os.path.isfile(product+"/manifest.safe"):
     report( "[ERROR] " + product+"/manifest.safe" + " NOT EXIST" )
     print "False"
     exit()
+
+
+#set thread pool
+for i in range(num_threads):
+    worker = Thread(target=checkMD5, args=(filesQ,))
+    worker.setDaemon(True)
+    worker.start()
+
 
 try:
     doc = libxml2.parseFile(product + "/manifest.safe")
@@ -51,19 +92,7 @@ try:
         filename_checksum = str(manifest.xpathEval('byteStream/checksum')[0].content)
         full_path = product+filename[1:]
 
-        if not os.path.isfile(full_path):
-            report( "[ERROR] " + full_path + " NOT EXIST" )
-            error = True
-            print "False"
-            exit()
-        else:
-            checksum = hashlib.md5(open(full_path, 'rb').read()).hexdigest()
-
-            if checksum != filename_checksum:
-                report( "[ERROR]" + product+filename[1:] + " NOT VALID" )
-                error = True
-                print "False"
-                exit()
+        filesQ.put(full_path)
 
     if error:
         report( "[ERROR] " + product + " NOT OK" )
@@ -73,6 +102,8 @@ try:
         report( product + " OK" )
         print "True"
         exit()
+
+    filesQ.join()
 
 except libxml2.parserError:
     report("manifest not valid ... ")
